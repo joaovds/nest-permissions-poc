10a99dd2449dc03d6b15ac588bf019c1746eb7df
João Victor da Silva <jv782063@gmail.com>
2025-02-17 20:28:33 -0300

    fix import

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { UsersService } from './users.service';
+
 import { Permissions } from '@/shared/permissions/decorators';
 import { PermissionsEnum } from '@/shared/permissions/enums';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @Permissions(PermissionsEnum.EDIT_PARISH, PermissionsEnum.DELETE_PARISH)
   testDatabase() {
     return this.usersService.testDatabase();
   }
